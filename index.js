fac6dda07a3582694d8089ee026666824bd217df
iannakim <dear.hyunji@gmail.com>
2020-10-03 22:11:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+
+class Polygon {
+    constructor(array) {
+        this.sides = array;
+        this.count = array.length;
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        return this.sides.reduce(function(a, b){
+            return a+b;
+        })
+
+    }
+
+}
+
+class Triangle extends Polygon{
+    get isValid() {
+        if (this.count == 3){
+            let side1 = this.sides[0]
+            let side2 = this.sides[1]
+            let side3 = this.sides[2]
+            return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+        }
+        else return;
+    }
+}
+
+class Square extends Polygon{
+    get isValid() {
+        if (this.count == 4){
+            let side1 = this.sides[0]
+            let side2 = this.sides[1]
+            let side3 = this.sides[2]
+            let side4 = this.sides[3]
+            return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+        }
+        else return;        
+    }
+
+    get area() {
+        if (this.count == 4){
+            return this.sides[0] * this.sides[0]
+        }
+        else return;
+    }
+}
